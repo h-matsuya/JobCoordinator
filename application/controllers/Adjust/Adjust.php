4fe80f4f83ae2f6ce466e2a3f56d7779cbd5f108
松谷 歩高 <>
2018-06-03 13:11:53 +0900

    メールスケジュール調整BOT修正

--- application/controllers/Adjust/Adjust.php
+++ application/controllers/Adjust/Adjust.php
@@ -56,7 +56,7 @@ class Adjust extends CI_Controller {
                 list($msg_id, $in_reply_to, $header_from) = $this->adjust_lib->get_msg_id_by_mail($msg->all_data);
                 $plain_to = $this->adjust_lib->fix_mail_format($msg->to, "<", ">");
                 $plain_from = $this->adjust_lib->fix_mail_format($msg->from, "<", ">");
-$msg->from = $msg->from. "@". $msg->from_domain;
+$msg->from = $plain_from. "@". $msg->from_domain;
                 var_dump($msg->from);
                 exit;
                 var_dump($msg->to);
